f2ab85ef86e1cbc6ceda950e0c24956756fa7baa
yewon <107863505+mynameisyewon@users.noreply.github.com>
2025-02-04 18:40:08 +0900

    Selected Text Read & Initialization Features
    선택된 텍스트 읽기 기능 & 초기화 기능

--- ios/AACProject/AACProject/ViewController.swift
+++ ios/AACProject/AACProject/ViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-
+//버튼 화면 띄우기//
     
+    /* 이미지 */
     @IBOutlet weak var selectedImageView1: UIImageView!
     
     @IBOutlet weak var selectedImageView2: UIImageView!
@@ -19,69 +21,180 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var selectedImageView4: UIImageView!
     
+    /* 텍스트 */
+    @IBOutlet weak var selectedImageView1Text: UILabel!
     
-    var num : [Int] = [0,0,0,0]
-    var recentSize : Int = 0
+    @IBOutlet weak var selectedImageView2Text: UILabel!
     
-    // 이미지 배열 (하단에 표시될 이미지들)
-    var sectionAimages : [UIImage] = [#imageLiteral(resourceName: "001"), #imageLiteral(resourceName: "002"), #imageLiteral(resourceName: "003"), #imageLiteral(resourceName: "004"), #imageLiteral(resourceName: "005"), #imageLiteral(resourceName: "006"), #imageLiteral(resourceName: "007"), #imageLiteral(resourceName: "008")]
+    @IBOutlet weak var selectedImageView3Text: UILabel!
+    
+    @IBOutlet weak var selectedImageView4Text: UILabel!
+    
+    
+    /* 이미지 & 텍스트 튜플 */
+    //이미지 배열
+    var sectionAimages : [UIImage] = [#imageLiteral(resourceName: "000"), #imageLiteral(resourceName: "001"), #imageLiteral(resourceName: "002"), #imageLiteral(resourceName: "003"), #imageLiteral(resourceName: "004"), #imageLiteral(resourceName: "005"), #imageLiteral(resourceName: "006"), #imageLiteral(resourceName: "007"), #imageLiteral(resourceName: "008")]
+    var recentSize = 0
+    var recentSizeMax = 4
+    
+    //튜플
+    struct imageTxtTupple {
+        var image : Int
+        var txt: String
+    }
 
-
+    var imageTxtList = [
+     imageTxtTupple(image: 0, txt: " "),
+     imageTxtTupple(image: 0, txt: " "),
+     imageTxtTupple(image: 0, txt: " "),
+     imageTxtTupple(image: 0, txt: " ")
+    ]
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("첫화면")
         updateSelectedImages()
+        updateSelectedImagesTxt()
        
     }
     
     
+    
+    /* ------------선택된 이미지 & 텍스트 업데이트----------*/
+    
+    // 선택된 이미지뷰 업데이트
     func updateSelectedImages() {
-        // 선택된 이미지뷰 업데이트
-        selectedImageView1.image = sectionAimages[num[0]]
-        selectedImageView2.image = sectionAimages[num[1]]
-        selectedImageView3.image = sectionAimages[num[2]]
-        selectedImageView4.image = sectionAimages[num[3]]
+        selectedImageView1.image = sectionAimages[imageTxtList[0].image]
+        selectedImageView2.image = sectionAimages[imageTxtList[1].image]
+        selectedImageView3.image = sectionAimages[imageTxtList[2].image]
+        selectedImageView4.image = sectionAimages[imageTxtList[3].image]
+    }
+    // 선택된 이미지뷰의 텍스트 업데이트
+    func updateSelectedImagesTxt() {
+        selectedImageView1Text.text = imageTxtList[0].txt
+        selectedImageView2Text.text = imageTxtList[1].txt
+        selectedImageView3Text.text = imageTxtList[2].txt
+        selectedImageView4Text.text = imageTxtList[3].txt
     }
     
+    
+    
+    
     @IBAction func Image1(_ sender: UIButton) {
-        num[recentSize] = 0
+        guard recentSize < recentSizeMax else {
+               showAlert(message: "최대 개수를 초과했습니다!")
+               return
+           }
+        
+        var txt = "1번텍스트"
+        imageTxtList[recentSize]=imageTxtTupple(image: 1, txt: txt)
         recentSize+=1
         print("버튼1눌림")
         updateSelectedImages()
+        updateSelectedImagesTxt()
     }
     
     @IBAction func Image2(_ sender: UIButton) {
-        num[recentSize] = 1
+        guard recentSize < recentSizeMax else {
+               showAlert(message: "최대 개수를 초과했습니다!")
+               return
+           }
+        
+        var txt = "2번텍스트"
+        imageTxtList[recentSize]=imageTxtTupple(image: 2, txt: txt)
         recentSize+=1
         print("버튼2눌림")
         updateSelectedImages()
+        updateSelectedImagesTxt()
     }
     
     @IBAction func Image3(_ sender: UIButton) {
-        num[recentSize] = 2
+        guard recentSize < recentSizeMax else {
+               showAlert(message: "최대 개수를 초과했습니다!")
+               return
+           }
+        
+        var txt = "3번텍스트"
+        imageTxtList[recentSize]=imageTxtTupple(image: 3, txt: txt)
         recentSize+=1
         print("버튼3눌림")
         updateSelectedImages()
+        updateSelectedImagesTxt()
     }
     
     @IBAction func Image4(_ sender: UIButton) {
-        num[recentSize] = 3
+        guard recentSize < recentSizeMax else {
+               showAlert(message: "최대 개수를 초과했습니다!")
+               return
+           }
+        
+        var txt = "4번텍스트"
+        imageTxtList[recentSize]=imageTxtTupple(image: 4, txt: txt)
         recentSize+=1
         print("버튼4눌림")
         updateSelectedImages()
+        updateSelectedImagesTxt()
     }
+    
+   /*   recentSizeMax 도달 경고창 (화면에 띄울 수 있는 크기 넘쳤을 경우)    */
+    func showAlert(message: String) {
+        if let vc = UIApplication.shared.connectedScenes.compactMap({ ($0 as? UIWindowScene)?.keyWindow?.rootViewController }).first {
+            let alert = UIAlertController(title: "경고", message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .default))
+            vc.present(alert, animated: true)
+        }
+    }
+    
+    
+    /*-------------초기화 버튼-------------*/
+    @IBAction func resetButton(_ sender: Any) {
+        recentSize = 0
+        
+        imageTxtList = [
+         imageTxtTupple(image: 0, txt: " "),
+         imageTxtTupple(image: 0, txt: " "),
+         imageTxtTupple(image: 0, txt: " "),
+         imageTxtTupple(image: 0, txt: " ")
+        ]
+        
+        updateSelectedImages()
+        updateSelectedImagesTxt()
+    }
+    
+    /*-----------선택된 그림의 텍스트 읽기-----------*/
+    
+    @IBAction func readingButtonAction(_ sender: Any) {
+        var readingSentence = " "
+        for i in 0..<recentSize {
+            readingSentence += imageTxtList[i].txt
+        }
+        
+        C_TTS.play(readingSentence)
+    }
+
+    
 }
 
-// 버튼 하나 누르면 바로 selectedImageView1로 넘어가게..
-//버튼 누른걸 배열로 저장 바로바로 하는거임
-//그럼 배열1이 생겼다 그럼 바로 선택이미지뷰로 넘어가고
-//버튼 하나 더 눌러서 배열2가 생겼다. 그럼 바로 선택이미지뷰2로 넘어가고
-//선택이미지뷰1=배열.0
-//선택이미지뷰2=배열.1
-//이런식으로 하면 되지 않으려나?
-//만약에 이미지 선택한거 취소하고 싶다.
-//-이미지를 누르면 삭제시키고 싶어.
-//이미지 터치 됐으면, 해당 이미지 배열 빈공간으로 만들고... 그럼 땡기는게 에반데
-//선택이미지뷰 뭔가 제공되는 라이브러리? 프레임워크? 있을 거 같다.
-//전체삭제 버튼도 있어야 겠다.
+class C_TTS {
+
+    static let speech = AVSpeechSynthesizer()
+    
+    static func play(_ string: String){
+        print("TTS 음성 재생 실행")
+        print("읽는 문장: \(string)")
+        
+        let utterance = AVSpeechUtterance(string: string)
+        utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR")
+        utterance.voice = AVSpeechSynthesisVoice(identifier: "Yuna")
+        utterance.rate = 0.5
+        utterance.pitchMultiplier = 0.8
+        speech.speak(utterance)
+        
+        try? AVAudioSession.sharedInstance().setCategory(.playback, options: .allowBluetooth)
+    }
+
+}
+
+
